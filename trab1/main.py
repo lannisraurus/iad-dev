47bcdb2461398ed57ca46768786250f01d880859
Lannisraurus <lannisraurus@gmail.com>
2025-02-21 23:10:26 +0000

    Placed mainWindow class in separate file

--- trab1/main.py
+++ trab1/main.py
@@ -1,22 +1,6 @@
-##################### Python Library Imports
-
-# UI
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (
-    QApplication,
-    QWidget,
-    QLineEdit,
-    QTextEdit,
-    QPushButton,
-    QVBoxLayout,
-    QHBoxLayout,
-    QLabel,
-    QStyle,
-)
-from PyQt5.QtGui import QPixmap
-import pyqtgraph    # For Data Visualization.
-
 # Communications
+from PyQt5.QtWidgets import QApplication # Qt App
+
 import os           # For USB access.
 import serial       # For USB communication.
 
@@ -24,90 +8,7 @@ import serial       # For USB communication.
 import numpy        # For Numerical Calculations.
 
 ##################### User defined functions (imports)
-
-##################### Main Programme Class
-
-# Class which inherits from QWidget class. Contains UI functionalities.
-
-class mainWindow(QWidget):
-
-    # Constructor
-    def __init__(self, *args, **kwargs):
-
-        super().__init__(*args, **kwargs)
-
-        # Set title
-        self.setWindowTitle('IAD - Project I (Group I)')
-
-        # Set Size
-        self.setGeometry(500, 500, 420, 220)
-
-        # UI Elements - Buttons
-        self.startButton = QPushButton('Run')
-        self.startButton.clicked.connect(self.startCommand)
-        self.stopButton = QPushButton('Interrupt')
-        self.stopButton.clicked.connect(self.stopCommand)
-        self.commandInfoButton = QPushButton('')
-        self.commandInfoButton.clicked.connect(self.infoCommand)
-        self.commandInfoButton.setIcon(self.style().standardIcon(QStyle.SP_MessageBoxInformation))
-
-        # UI Elements - Pixmaps
-        self.groupLogoPixmap = QPixmap('assets/logo.png')
-        self.groupLogoPixmap = self.groupLogoPixmap.scaled(250, 250, Qt.KeepAspectRatio)
-
-        # UI Elements - Labels
-        self.commandInputLabel = QLabel('Command:')
-        self.groupLogoLabel = QLabel()
-        self.groupLogoLabel.setPixmap(self.groupLogoPixmap)
-
-        # UI Elements - Line/Text Edits
-        self.commandInputLine = QLineEdit()
-        self.commandOutputLine = QTextEdit()
-        self.commandOutputLine.setReadOnly(True)
-        self.commandOutputLine.setMinimumSize(400,250)
-        self.logText("*****************************")
-        self.logText("RPi - Arduino Interface (Log)")
-        self.logText("*****************************")
-        self.logText("")
-
-
-        
-        # Create a layouts
-        self.mainLayout = QVBoxLayout()
-        self.setLayout(self.mainLayout)
-        self.topLayout = QHBoxLayout()
-        self.bottomLayout = QHBoxLayout()
-        self.mainLayout.addLayout(self.topLayout)
-        self.mainLayout.addLayout(self.bottomLayout)
-
-        # UI Elements - Top Layout 
-        self.topLayout.addWidget(self.groupLogoLabel)
-        self.topLayout.addWidget(self.commandOutputLine)
-        
-        # UI Elements - Main Layout
-        self.mainLayout.addWidget(self.startButton)
-        self.mainLayout.addWidget(self.stopButton)
-        
-        # UI Elements - Bottom Layout
-        self.bottomLayout.addWidget(self.commandInputLabel)
-        self.bottomLayout.addWidget(self.commandInputLine)
-        self.bottomLayout.addWidget(self.commandInfoButton)
-        
-        # Show window
-        self.show()
-    
-    # UI functions
-    def logText(self,msg): 
-        self.commandOutputLine.setPlainText(self.commandOutputLine.toPlainText() + msg + "\n")
-
-    def startCommand(self):
-        self.logText("*** Starting Acquisition.")
-
-    def stopCommand(self):
-        self.logText("*** Ending Acquisition.")
-
-    def infoCommand(self):
-        self.logText("* Opening Info Window.")
+from mainWindow import mainWindow
 
 ##################### Main Programme Function
 
