1ed4fed21bc348d8232963ff55d111d4a2bc7400
LannisRaurus <lannisraurus@gmail.com>
2025-02-26 14:54:21 +0000

    read fixing...

--- trab1/arduinoComms.py
+++ trab1/arduinoComms.py
@@ -80,7 +80,7 @@ class arduinoComms:
             return self.tryOpeningIntToStr(tryOpen)
         message = ""
         start_time = time.time()
-        while self.serialObject.inWaiting > 0 and time.time()-start_time <= self.timeoutSeconds:
+        while self.serialObject.inWaiting() > 0 and time.time()-start_time <= self.timeoutSeconds:
             message += self.serialObject.readline().decode('utf-8')
         if time.time()-start_time >= self.timeoutSeconds:
             return "TIMEOUT ("+str(time.time()-start_time)+" seconds).\n" 
