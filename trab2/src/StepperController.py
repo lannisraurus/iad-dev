c834f76728be10dec437cb647ef831ed5a5651ec
MIBismuth <jodulopes@gmail.com>
2025-04-02 18:48:27 +0100

    altAz

--- trab2/src/StepperController.py
+++ trab2/src/StepperController.py
@@ -14,7 +14,7 @@ import time
 ### Class Definition
 class StepperController():
     # Constructor
-    def __init__(self,initialAlt=0,initialAz=0):                                         
+    def __init__(self,initialAlt=0.0,initialAz=0.0):                                         
         # Define motor driver pins
         self.coil_A_1_pin = OutputDevice(24) # pink
         self.coil_A_2_pin = OutputDevice(4)  # orange
@@ -36,8 +36,8 @@ class StepperController():
         self.Seq[6] = [0,0,1,0]
         self.Seq[7] = [0,1,1,0]
 
-        self.alt = initialAlt
-        self.az = initialAz
+        self.alt = round(512*8*32*initialAlt/360/2)
+        self.az = round(512*8*32*initialAz/360/2)
 
     # lower pins
     def stepAz(self,indexSeq):
@@ -53,39 +53,33 @@ class StepperController():
         self.coil2_B_1_pin.value = self.Seq[indexSeq][2]
         self.coil2_B_2_pin.value = self.Seq[indexSeq][3]
     
-    def moveToAz(self, az, delay):
+    def moveToAz(self, az, delay=0.001):
+        az=round(512*8*32*az/360/2)
         for i in range(self.az,az, 1 if self.az < az else -1):
             self.stepAz((8-i)%8)
             time.sleep(delay)
         self.az=az
         
-    def moveToAlt(self, alt, delay):
-        for i in range(self.alt,alt):
+    def moveToAlt(self, alt, delay=0.001):
+        az=round(512*8*32*alt/360/2)
+        for i in range(self.alt,alt, 1 if self.alt < alt else -1):
             self.stepAlt(i%8)
             time.sleep(delay)   
         self.alt=alt  
-"""
-def backwards1(delay, steps):
-    for i in range(steps):
-        for j in reversed(range(StepCount)):
-            setStep1(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
 
-def backwards2(delay, steps):
-    for i in range(steps):
-        for j in reversed(range(StepCount)):
-            setStep2(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
+    def moveTo(self, az, alt, delay=0.001):
+        self.moveToAz(az,delay)
+        self.moveToAlt(alt,delay)
 
-    def moveTo(alt, az, timePerStep):
-        if az > self.az
-        for i in range(az-self.az):
-            for j in range(StepCount):
-                setStep1(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-                time.sleep(delay)
-    """
-
+    def centerCoords(self, currAz, currAlt):
+        self.az = round(currAz)
+        self.alt = round(currAlt)
 
 A = StepperController()
-A.moveToAlt(512*8*32,1./1000.)
-A.moveToAz(512*8*32,1./1000.)
+#A.moveToAlt(512*8*32,1./1000.)
+A.moveToAz(90)
+A.moveToAz(-90)
+A.moveToAz(90)
+A.moveToAz(-90)
+A.moveToAz(45)
+A.moveToAz(-45)
