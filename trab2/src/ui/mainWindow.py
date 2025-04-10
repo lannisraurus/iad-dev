d7afc2feda32b2a19f21c7d9995ad7b4e8637cd9
João Camacho <121614992+LannisRaurus@users.noreply.github.com>
2025-04-10 17:37:47 +0100

    MIAU

--- trab2/src/ui/mainWindow.py
+++ trab2/src/ui/mainWindow.py
@@ -560,9 +560,8 @@ class mainWindow(QWidget):
             self.logText("one " if self.alignmentDropdown.currentIndex() == 0 else "at least three (separated by commas) ")
             self.logText("of the provided objects for alignment, or provide the name of your preferred object(s). " + \
                         "Type exit to cancel alignment.\n" +\
-                        "Below are the recommended objects:\n" + str(objs[:10]))
+                        "Below are the recommended objects:\n" + str(objs[:10] + "\n"))
             self.receiverForText = self.alignmentRoutine3
-            self.waitingForText = True
         else:
             self.logText("Input not recognised, please type either ok or exit.\n")
 
