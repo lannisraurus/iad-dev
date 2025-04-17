88c761a30c4db9230ad9dabd53662368c30335d4
João Camacho <121614992+LannisRaurus@users.noreply.github.com>
2025-04-17 10:50:07 +0100

    Fixed stepper controller alignment position update

--- trab2/src/ui/mainWindow.py
+++ trab2/src/ui/mainWindow.py
@@ -754,7 +754,7 @@ class mainWindow(QWidget):
                 self.alignList = []
                 self.logText("> Alignment complete. \n" + "-----------------\n\n")
                 self.receiverForText = None
-                self.updateAltAzLabel()
+                self.updateAltAzLabel(self.stepperController.getCoords())
                 return
             
             self.logText(f"> Using {self.alignList[-self.itemsInAlign+1]} to align, please point to it and type ok when finished, or type exit to cancel.\n")
